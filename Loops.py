71c9d681b5dbf79585fe84e2d42f385a75a366b9
MadhuKumarGowda <madhukumar.gowda@gmail.com>
2025-01-14 12:19:30 -0500

    For loop demostration

---
+++ Loops.py
@@ -0,0 +1,19 @@
+# Demonstrate about loops and its syntax
+
+# Below loop will print the output from 0 to 9
+# by default increment value is 1
+print("--------------------")
+for x in range(0,10):
+    print(x)
+print("--------------------")
+    
+# below is the example for for loop with one parameter
+for x in range(5): # 5 is target number, it start from 0 and increase by 1
+    print(x)
+print("--------------------")
+
+# If you want add increment operator by 2/3/4....
+# Below loop will increase the number by 3 as i mentioned 3 as third parameter
+for x in range(0,10,3): # 0 is the start, 10 is the target, and 3 is increment 
+    print(x)
+print("--------------------")
