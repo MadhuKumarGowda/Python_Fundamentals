c0524ec2310215746ad0c32cc63655ca3b61c662
MadhuKumarGowda <madhukumar.gowda@gmail.com>
2025-01-31 01:52:37 -0500

    Added Flask based web apps for weather app

--- weather.py
+++ weather.py
@@ -8,6 +8,11 @@ load_dotenv()
 def get_Current_weather():
     print("\n*** Get Current Weather Conditions ***\n")
     city = input("\nEnter your city: \n")
+    
+    # check for empty string / string with spaces
+    if not bool(city.strip()):
+        city = "Mysore"       
+    
     request_url = f'https://api.openweathermap.org/data/2.5/weather?appid={os.getenv("API_KEY")}&q={city}&units=metric'
     response = requests.get(request_url)
     if response.status_code == 200:
@@ -18,7 +23,15 @@ def get_Current_weather():
         print(f"Feels like: {data['main']['feels_like']}°C")
         print(f"Description: {data['weather'][0]['description']}")
     else:
-        print(f"Error: {response.status_code}") 
+        print(f"Error: {response.status_code}")
+        
+def get_current_weather_byCity(city="Mysore"):
+     request_url = f'https://api.openweathermap.org/data/2.5/weather?appid={os.getenv("API_KEY")}&q={city}&units=metric'
+     weather_data = requests.get(request_url).json()
+     
+     return weather_data
+    
 
 if __name__ == "__main__":
     get_Current_weather()
+     
